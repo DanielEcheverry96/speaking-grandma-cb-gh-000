415b55cf01907e8ed4830fe725be41f3bc98e7f7
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-01 05:11:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phraseMayus
-    "NO, NOT SINCE 1938"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
